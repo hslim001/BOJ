65c44123323abe98467b9788894431c089b5001d
Comet <54059326+hslim001@users.noreply.github.com>
2025-04-06 17:27:05 +0900

    [Gold V] Title: LCS, Time: 188 ms, Memory: 56604 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,22 @@
+def find():
+    first = '.' + input().strip()
+    second = '.' + input().strip()
+    if len(first) < len(second):
+        first, second = second, first
+    dp = [[0] * (len(second)) for _ in range(len(first))]
+
+    for i in range(1, len(first)):
+        temp = dp[i - 1][0]
+        for j in range(1, len(second)):
+            if first[i] == second[j]:
+                dp[i][j] = temp + 1
+
+            else:
+                dp[i][j] = dp[i - 1][j]
+            temp = max(temp, dp[i - 1][j])
+    return max(dp[-1])
+
+
+
+
+print(find())
