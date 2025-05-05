ca6dd6fd71619179213bba56675165e0cd286020
Comet <54059326+hslim001@users.noreply.github.com>
2025-05-05 22:08:48 +0900

    [Gold V] Title: 동전 2, Time: 160 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,16 +1,18 @@
-num, limit = map(int, input().split())
-coins = []
-for _ in range(num):
-    temp = int(input())
-    if temp <= limit:
-        coins.append(temp)
-
-dp = [99999] * (limit + 1)
-dp[0] = 0
-for i in range(1, limit + 1):
-    for coin in coins:
-        if coin <= i:
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-if dp[limit] == 99999:
-    dp[limit] = -1
-print(dp[limit])
+def find():
+    num, limit = map(int, input().split())
+    coins = []
+    for _ in range(num):
+        temp = int(input())
+        if temp <= limit:
+            coins.append(temp)
+    
+    dp = [99999] * (limit + 1)
+    dp[0] = 0
+    for i in range(1, limit + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    if dp[limit] == 99999:
+        dp[limit] = -1
+    print(dp[limit])
+find()
