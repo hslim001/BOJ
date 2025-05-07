fbe79e7c78f3b4cba7afb97ffc9831beb3585175
Comet <54059326+hslim001@users.noreply.github.com>
2025-05-07 23:55:56 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 196 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.cc
+++ 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.cc
@@ -7,44 +7,39 @@ using namespace std;
 struct Load
 {
   int city;
-  unsigned long long length;
-  bool operator<(const Load& other) const {
-    return length > other.length;
-  }
+  int length;
 };
 
 int main() {
   //ifstream cin("input.txt");
-  ios::sync_with_stdio(false); cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
   int city_num, load_num, interview_num;
   cin >> city_num >> load_num >> interview_num;
   vector<vector<Load>> graph(city_num + 1);
   for (int i=0; i < load_num; i++){
-    int u, v;
-    unsigned long long len;
+    int u, v, len;
     cin >> u >> v >> len;
     graph[v].push_back({u, len});
   }
   vector<unsigned long long> visited(city_num + 1, ULLONG_MAX);
-  priority_queue<Load> que;
+  deque<int> que;
   for (int i=0; i < interview_num; i++){
     int interview_place;
     cin >> interview_place;
     visited[interview_place] = 0;
-    que.push({interview_place, 0});
+    que.push_back(interview_place);
   }
   
   int current = 0;
-  unsigned long long cur_len = 0;
   while (!que.empty()){
-    current = que.top().city;
-    cur_len = que.top().length; que.pop();
-    if (cur_len > visited[current]) continue;
+    current = que.front(); que.pop_front();
     for (int i = 0; i < graph[current].size(); i ++){
       unsigned long long temp = graph[current][i].length + visited[current];
       if (temp < visited[graph[current][i].city]){
         visited[graph[current][i].city] = temp;
-        que.push({graph[current][i].city, temp});
+        que.push_back(graph[current][i].city);
       }
     }
   }
@@ -54,7 +49,7 @@ int main() {
       min_len = i;
     }
   }
-  cout << min_len << endl << visited[min_len];
+  cout << min_len << endl << visited[min_len] << endl;
   // for (int i = 1; i < city_num + 1; i++){
   //   cout << visited[i] << " ";
   // }
