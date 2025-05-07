76b5907d53b982f0506501c6f48ba93f1bd0b77a
Comet <54059326+hslim001@users.noreply.github.com>
2025-05-07 23:50:00 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 248 ms, Memory: 21372 KB -BaekjoonHub

--- 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.cc
+++ 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.cc
@@ -7,36 +7,44 @@ using namespace std;
 struct Load
 {
   int city;
-  int length;
+  unsigned long long length;
+  bool operator<(const Load& other) const {
+    return length > other.length;
+  }
 };
 
 int main() {
   //ifstream cin("input.txt");
+  ios::sync_with_stdio(false); cin.tie(0);
   int city_num, load_num, interview_num;
   cin >> city_num >> load_num >> interview_num;
   vector<vector<Load>> graph(city_num + 1);
   for (int i=0; i < load_num; i++){
-    int u, v, len;
+    int u, v;
+    unsigned long long len;
     cin >> u >> v >> len;
     graph[v].push_back({u, len});
   }
   vector<unsigned long long> visited(city_num + 1, ULLONG_MAX);
-  deque<int> que;
+  priority_queue<Load> que;
   for (int i=0; i < interview_num; i++){
     int interview_place;
     cin >> interview_place;
     visited[interview_place] = 0;
-    que.push_back(interview_place);
+    que.push({interview_place, 0});
   }
   
   int current = 0;
+  unsigned long long cur_len = 0;
   while (!que.empty()){
-    current = que.front(); que.pop_front();
+    current = que.top().city;
+    cur_len = que.top().length; que.pop();
+    if (cur_len > visited[current]) continue;
     for (int i = 0; i < graph[current].size(); i ++){
       unsigned long long temp = graph[current][i].length + visited[current];
       if (temp < visited[graph[current][i].city]){
         visited[graph[current][i].city] = temp;
-        que.push_back(graph[current][i].city);
+        que.push({graph[current][i].city, temp});
       }
     }
   }
@@ -46,7 +54,7 @@ int main() {
       min_len = i;
     }
   }
-  cout << min_len << endl << visited[min_len] << endl;
+  cout << min_len << endl << visited[min_len];
   // for (int i = 1; i < city_num + 1; i++){
   //   cout << visited[i] << " ";
   // }
